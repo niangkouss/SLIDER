e4d3fa2862f26887fe7452c262b2d637f8d62214
niangkouss <takeevernote@163.com>
2017-08-30 00:59:01 +0800

    目标组件的效果: 1.引入即可使用 2.images要外部引入 3.尽可能实现多的功能,使用组件的时候判断是否开启此功能

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by sole l on 2017/8/30.
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Slider from './components/slider'
+let images=[
+    {src:require('./img/1.jpg')},
+    {src:require('./img/2.jpg')},
+    {src:require('./img/3.jpg')}
+];
+ReactDOM.render((
+    <Slider
+        images={images}
+        interval={2}
+        speed={1}
+        autoplay={true}
+        pause={true}
+        arrows={true}
+        dots={true}
+    />
+),document.querySelector('#root'));
